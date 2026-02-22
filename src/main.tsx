a61811a526f1f9229afd879c211932d263f5d69e
Davi Peperaio <peperaiodp126@gmail.com>
2026-02-22 11:48:58 -0300

    Fix PWA registration for production

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 
-registerSW({
-  immediate: true,
-});
+if (import.meta.env.PROD) {
+  registerSW({
+    immediate: true,
+  });
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
